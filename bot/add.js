22d98f55fd81f9f61acb28161be4a9b99a054c8e
Pouria Ezzati <ezzati.upt@gmail.com>
2017-06-29 12:48:43 +0430

    fixed undefined for name

--- bot/add.js
+++ bot/add.js
@@ -8,9 +8,12 @@ const handleAdd = async (ctx) => {
             await user.save();
             return ctx.replyWithMarkdown('Please enter the username of the channel. \n\nFor example *@example*');
         } else {
+            const firstName = ctx.message.from.first_name || '';
+            const lastName = ctx.message.from.last_name || '';
+
             const newUser = new User({
                 userId: ctx.message.from.id,
-                name: `${ctx.message.from.first_name} ${ctx.message.from.last_name}`,
+                name: `${firstName} ${lastName}`,
                 username: ctx.message.from.username,
                 state: {
                     add: true
